92ca978133c3e5605fe3e5674ac87b33ea05f934
chenzhuo <joychin@foxmail.com>
2017-02-13 23:25:20 +0800

    Initial commit

---
+++ CTP/CTP.h
@@ -0,0 +1,13 @@
+// CTP.h
+
+#pragma once
+
+using namespace System;
+
+namespace CTP {
+
+	public ref class Class1
+	{
+		// TODO:  在此处添加此类的方法。
+	};
+}
---
+++ CTPLib/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CTP;
+namespace CTPLib
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(CTP.EnumBatchStatusType.NoUpload);
+            Console.ReadKey();
+        }
+    }
+}
